bfc4f1a68418a90cc5712081151e1c2ed5ae58fa
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2024-08-14 19:09:09 +0530

    This is the first spring boot commit

---
+++ quickStart/src/test/java/com/firstSpringBoot/quickStart/QuickStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstSpringBoot.quickStart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
